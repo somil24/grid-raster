c2699a088ce44e670a14f62c739cb0fa14e43eb6
somil24 <somil2402@gmail.com>
2022-04-10 23:16:02 +0530

    done

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 
 const Server = require("socket.io");
-const covidData=require('./assets/covidData');
-const RESULT = require("./result");
+const csvToJson = require("convert-csv-to-json");
 
 const io =  Server(5000, {
   cors: {
@@ -9,16 +8,29 @@ const io =  Server(5000, {
   }
 });
 
-const result = JSON.parse(JSON.stringify(RESULT));
-console.log(result);
-let index=0;
+
+const json = csvToJson.getJsonFromCsv("./assets/covid_data.csv");
+console.log(json);
+
+let index = 0;
 io.on("connection", async (socket) => {
-
   setInterval(() => {
-    if(index == DATA.length) return;
-    const data = [];
-    data.push(DATA[index++]);
-    socket.emit("graphData", data);
-
-  },2000)
+    if (index < json.length) {
+      socket.emit("graphData", json[index++]);
+    } else return;
+  }, 2000);
 });
+//const result = JSON.parse(JSON.stringify(RESULT));
+//console.log(result);
+
+// let index=0;
+// io.on("connection", async (socket) => {
+
+//   setInterval(() => {
+//     if(index == DATA.length) return;
+//     const data = [];
+//     data.push(DATA[index++]);
+//     socket.emit("graphData", data);
+
+//   },2000)
+// });
